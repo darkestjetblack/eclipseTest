c40673158c3351929ab2a26cc6d92683aa335967
darkestjetblack <36657072+darkestjetblack@users.noreply.github.com>
2018-02-21 10:57:07 +0900

    Update SayHello.java
    Git Hub에서 6번 줄에 소스 한 줄 추가 하고 커밋함

--- GitProject/src/kr/co/test/SayHello.java
+++ GitProject/src/kr/co/test/SayHello.java
@@ -3,5 +3,6 @@ package kr.co.test;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("HELLO!!!!!! How have you been??? I'm fine.... HELLO!!!!!");
+		System.out.println("These day I was sick, but now I'm fine");
 	}
 }
