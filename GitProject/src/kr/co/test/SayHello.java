74bea43c6066313aa2339aecd2db056365d58a41
darkestjetblack <36657072+darkestjetblack@users.noreply.github.com>
2018-02-21 11:09:20 +0900

    서버에서 수정했다
    서버에서 8번째 줄에 주고 커밋했음

--- GitProject/src/kr/co/test/SayHello.java
+++ GitProject/src/kr/co/test/SayHello.java
@@ -5,5 +5,6 @@ public class SayHello {
 		System.out.println("HELLO!!!!!! How have you been??? I'm fine.... HELLO!!!!!");
 		System.out.println("Why do you say nothing?? I'm curious why...");
 		System.out.println("These days I was sick, but now I'm fine");
+		System.out.println("Well..");
 	}
 }
