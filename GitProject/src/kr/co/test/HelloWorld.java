8e4b5427e914b5bbdc323f999f3598963a4e9f3c
SW <seosowon@gmail.com>
2018-02-21 10:11:39 +0900

    2nd commit by master

--- GitProject/src/kr/co/test/HelloWorld.java
+++ GitProject/src/kr/co/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("worked in Second branch");
+		System.out.println("After merged with second branch, this line was added by master branch.");
 	}
 }
