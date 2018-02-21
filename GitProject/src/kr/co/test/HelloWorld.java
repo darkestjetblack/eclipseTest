21a38060b5237861fd599675aba6598a165fbf04
SW <seosowon@gmail.com>
2018-02-21 10:13:07 +0900

    2nd commit in second branch

--- GitProject/src/kr/co/test/HelloWorld.java
+++ GitProject/src/kr/co/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("worked in Second branch");
+		System.out.println("This is written by Second branch.");
 	}
 }
